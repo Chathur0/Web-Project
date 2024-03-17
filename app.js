93074f1d4485eb77cca91332277c6ddc10f6e044
Chathur0 <145315287+Chathur0@users.noreply.github.com>
2024-03-17 22:42:23 +0530

    error fixed

--- app.js
+++ app.js
@@ -620,3 +620,22 @@ function showHMore(index) {
         }
     });
 }
+
+var scrollButton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
